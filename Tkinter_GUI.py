826fc12fb8a47ba7ccf3ccb85d993c0c8b5d91b0
Rohan Sahuji <sahujir1018@gmail.com>
2023-06-02 13:20:08 +0530

    Shortcut keys added, textboxes clearing, multiple commands for buttons and Radiobuttons for search results

--- Tkinter_GUI.py
+++ Tkinter_GUI.py
@@ -18,7 +18,7 @@ class School_Data:
     def home(self):
         self.clear_screen()
 
-        self.label = tk.Label(self.root, text="Sample School Data", font=("Arial", 24))
+        self.label = tk.Label(self.root, text="Sample School Data", font=("Calibri", 24))
         self.label.pack(padx=20, pady=20)
 
         image = Image.open("school_image.jpg")
@@ -27,16 +27,27 @@ class School_Data:
         image_label = tk.Label(self.root, image=self.photo)
         image_label.pack(padx=10, pady=10)
 
-        self.add_button("Add", self.add)
-        self.add_button("Search", self.search)
-        self.add_button("Extra", self.extra)
+        self.homeframe = tk.Frame(self.root)
+        self.homeframe.pack(padx=20, pady=20)
 
-    def add_button(self, text, *commands):
-        button = tk.Button(self.root, text=text, font=("Arial", 16))
-        button.pack(padx=10, pady=10)
-        for command in commands:
-            button.config(command = lambda c=command: c())
+        self.add_button_in_frame(self.homeframe,"Add",0,0, self.add)
+        self.add_button_in_frame(self.homeframe,"Search",0,1, self.search)
+        self.add_button_in_frame(self.homeframe,"Extra",0,2, self.extra)
+
+    def add_button_in_frame(self, parent, text, row, col, *commands):
+        button = tk.Button(parent, text=text, font=("Arial", 14))
+        button.grid(row=row, column=col)
+        for cmd in commands:
+            button.config(command = lambda c=cmd: c())
         return button
+
+    def add_button(self, text, commands):
+        def execute_commands():
+            for command in commands:
+                command()
+        
+        button = tk.Button(self.root, text=text, font=("Arial", 14), command=execute_commands)
+        button.pack(padx=10, pady=10)
     
     #The add method contains all widgets and functions to be applied in 'ADD' screen of the 
     # window and is accessed after pressing Add button on the home screen
@@ -54,8 +65,8 @@ class School_Data:
         self.create_label_and_entry(self.addframe, "Age", 1, "Age", "")
         self.create_label_and_entry(self.addframe, "Class", 2, "Class", "")
 
-        self.add_button("Add", self.connection_add)
-        self.add_button("Home", self.home)
+        self.add_button("Add", [self.connection_add])
+        self.add_button("Home", [self.home])
 
     def search(self):
         # Firsty remove widgets of the homescreen
@@ -76,17 +87,23 @@ class School_Data:
         self.search_value = tk.Text(self.searchframe, height=1, font=("Arial", 12))
         self.search_value.grid(row=1, column=1)
 
-        self.add_button("Search", self.connection_search)
-        self.add_button("Home", self.home)
+        self.add_button("Search", [self.connection_search])
+        self.add_button("Home", [self.home])
 
     def create_label_and_entry(self, parent, text, row, entry_name, default_value):
         label = tk.Label(parent, text=text, font=("Arial", 14))
         label.grid(sticky=tk.W + tk.E)
         entry = tk.Text(parent, height=1, font=("Arial", 12))
+        entry.bind("<KeyPress>", self.shortcut)
         entry.insert("1.0", default_value)
         entry.grid(row=row, column=1, sticky=tk.W + tk.E)
         setattr(self, entry_name, entry)
   
+    def clear_text(self):
+        text_entry = [widget for widget in self.addframe.winfo_children() if isinstance(widget, tk.Text)]
+        for element in text_entry:
+            element.delete('1.0', 'end')
+    
     def create_connection(self):
         try:
             connection = sqlite3.connect(r"c:\Users\rsahu\Documents\git_files\Repo1\data.db")
@@ -103,18 +120,22 @@ class School_Data:
 
             data_entry = '''CREATE TABLE IF NOT EXISTS Stud_Data (name TEXT, age INT, class INT)'''
             connection.execute(data_entry)
-            
-            cursor = connection.cursor()
             data_insert = '''INSERT INTO Stud_Data (name, age, class) VALUES (?,?,?)'''
             data_insert_tuple = (
                 self.Name.get('1.0', 'end-1c'),
                 self.Age.get('1.0', 'end-1c'),
                 self.Class.get('1.0', 'end-1c')
                 )
-            cursor.execute(data_insert, data_insert_tuple)
-            connection.commit()
-            connection.close()
-            messagebox.showinfo(title='Congratulations!', message='Entry added Successfully!')
+            
+            if '' in data_insert_tuple:
+                messagebox.showinfo(title='Error', message='Kindly fill in all the details')
+            else:
+                cursor = connection.cursor()
+                cursor.execute(data_insert, data_insert_tuple)
+                connection.commit()
+                connection.close()
+                messagebox.showinfo(title='Congratulations!', message='Entry added Successfully!')
+                self.clear_text()
         
         except Error as e:
             print(e)
@@ -145,10 +166,22 @@ class School_Data:
         self.dispframe.pack(fill = 'x')
 
         for i, row in enumerate(info, start=1):
+            rb = tk.Radiobutton(self.dispframe)
+            rb.grid(row=i, column=0)
             for j, value in enumerate(row):
                 label = tk.Label(self.dispframe, text=value, font=("Arial", 14))
-                label.grid(row=i, column=j)
+                label.grid(row=i, column=j+1)
 
+    def shortcut(self, event):
+        if event.keysym == 'Return' and event.state == 0:
+            self.connection_add()
+
+        if event.keysym == 'Tab' and event.state == 0:
+            current_widget = event.widget
+            current_widget.tk_focusNext().focus()
+            return 'break'
+        
+    
     def extra(self):
         # Add your additional functionality here
         pass
